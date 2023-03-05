cb9a5f95d1d09f3378376260e63163386be908a6
KaioBerlt <kaioberlt@gamil.com>
2023-03-05 08:36:52 -0300

    Fix: metodos router

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -9,7 +9,7 @@ router.get("/findAll", usuarioController.findAllUsersController);
 //rotas POST
 router.post("/create", usuarioController.creatUserController);
 router.post("/addAddress/:id", usuarioController.addUserAdressController);
-router.post("/addFavProduct/:id", usuarioController.addUserFavProduct);
+router.post("/addFavProduct/:id", usuarioController.addUserFavProductController);
 
 //rotas PUT
 router.put("/update/:id", usuarioController.updateUserController);
@@ -17,6 +17,6 @@ router.put("/update/:id", usuarioController.updateUserController);
 //rotas DELETE
 router.delete("/remove/:id", usuarioController.removeUserController);
 router.delete("/removeAddress", usuarioController.removeUserAdressController);
-router.delete("/removeFavProduct", usuarioController.removeUserFavProduct);
+router.delete("/removeFavProduct", usuarioController.removeUserFavProductController);
 
 module.exports = router;
