7f63666d51401a391db93e69546804eee49043b4
KaioBerlt <kaioberlt@gamil.com>
2023-03-10 06:53:27 -0300

    Feat: autenticação funcionando

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const connectToDatabase = require ("./src/database/database"); //arquivo de conexão com o banco
 const usuario = require ("./src/router/usuario.router");//arquivo de rota do usuario
+const auth = require ("./src/router/auth.router");//arquivo de rota de login
+
 const app = express();
 connectToDatabase ();//conectando com o banco
 
@@ -14,7 +16,9 @@ app.get("/", (req,res) => {
         message: "Bem vindo"
     });
 })
-app.use("/usuario", usuario);
+app.use("/usuario", usuario);//chamando rotas de usuario
+app.use("/auth", auth);//chamando rotas de auth
+
 app.listen(port, () =>{
     console.log(`Servidor rodando em: http://localhost:${port}`);
 })
