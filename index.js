2d7ca2ad75c38917b687d9a65b7db6261e7437f4
KaioBerlt <kaioberlt@gamil.com>
2023-03-11 08:03:57 -0300

    Feat + Fix produtos e fix testes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require ("dotenv").config();
 const connectToDatabase = require ("./src/database/database"); //arquivo de conexão com o banco
 const usuario = require ("./src/router/usuario.router");//arquivo de rota do usuario
 const auth = require ("./src/router/auth.router");//arquivo de rota de login
+const produto = require ("./src/router/produto.router");//arquivo de rota de produtos
 
 const app = express();
 connectToDatabase ();//conectando com o banco
@@ -19,6 +20,7 @@ app.get("/", (req,res) => {
 })
 app.use("/usuario", usuario);//chamando rotas de usuario
 app.use("/auth", auth);//chamando rotas de auth
+app.use("/produto", produto);//chamando rotas de produto
 
 app.listen(port, () =>{
     console.log(`Servidor rodando em: http://localhost:${port}`);
--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //conexão com o Mongo DB 
 function connectToDatabase () {
-    mongoose.connect(process.env.URLDATABASE, {
+    mongoose.connect("mongodb://127.0.0.1:27017/Projeto" /*process.env.URLDATABASE*/, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
