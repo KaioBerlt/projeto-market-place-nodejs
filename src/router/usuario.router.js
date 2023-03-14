5cb3a8a95c3ef3522df05d9badd5bb8e73949819
KaioBerlt <kaioberlt@gamil.com>
2023-03-14 07:55:54 -0300

    Feat: Validação Usuario, Fix usuario router

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -3,22 +3,23 @@ const router = express.Router();
 const usuarioController = require ("../controller/usuario.controller");
 
 const authMiddleware = require ("../middleware/auth.middleware");
+const { validaUsuario } = require("../middleware/validacao.middleware");
 
 //rotas GET
-router.get("/findById/:id", authMiddleware ,usuarioController.findUserByIdController );
-router.get("/findAll", usuarioController.findAllUsersController);
+router.get("/findById/:id", authMiddleware, usuarioController.findUserByIdController );
+router.get("/findAll", authMiddleware, usuarioController.findAllUsersController);
 
 //rotas POST
-router.post("/create", usuarioController.createUserController);
-router.post("/addAddress/:id", usuarioController.addUserAdressController);
-router.post("/addFavProduct/:id", usuarioController.addUserFavProductController);
+router.post("/create", validaUsuario, usuarioController.createUserController);
+router.post("/addAddress/:id", authMiddleware, usuarioController.addUserAdressController);
+router.post("/addFavProduct/:id", authMiddleware, usuarioController.addUserFavProductController);
 
 //rotas PUT
-router.put("/update/:id", usuarioController.updateUserController);
+router.put("/update/:id", authMiddleware, validaUsuario, usuarioController.updateUserController);
 
 //rotas DELETE
-router.delete("/remove/:id", usuarioController.removeUserController);
-router.delete("/removeAddress/:id", usuarioController.removeUserAdressController);
-router.delete("/removeFavProduct/:id", usuarioController.removeUserFavProductController);
+router.delete("/remove/:id", authMiddleware, usuarioController.removeUserController);
+router.delete("/removeAddress/:id", authMiddleware, usuarioController.removeUserAdressController);
+router.delete("/removeFavProduct/:id", authMiddleware, usuarioController.removeUserFavProductController);
 
 module.exports = router;
