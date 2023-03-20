d3f3bdef49d9db50ee16b9f50d87db24703f06d3
KaioBerlt <kaioberlt@gamil.com>
2023-03-20 06:58:13 -0300

    Feat: Implementação CORs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 require ("dotenv").config();
+const cors = require("cors");
 const connectToDatabase = require ("./src/database/database"); //arquivo de conexão com o banco
 
 const usuario = require ("./src/router/usuario.router");//arquivo de rota do usuario
@@ -13,6 +14,15 @@ const docs = require("./src/router/docs.router");//arquivo de rota documentacao
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cors(
+    {
+        origin: [
+            "localhost:3001",
+            "localhost:3002"
+        ],
+        methods: ["GET", "POST", "PUT", "PATCH","DELETE"]
+    }
+));
 
 connectToDatabase ();//conectando com o banco
 
